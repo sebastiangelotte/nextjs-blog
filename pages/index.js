b4b9b377e8af269b00d1667c7b5ff7b25777c2be
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2020-10-24 14:43:15 +0200

    Testing builds on Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <Link href="/posts/firs-post">
+            <a>Next.js!</a>
+          </Link>
         </h1>
 
         <p className="description">
@@ -54,7 +57,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -205,5 +208,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
